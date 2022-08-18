a899d9fac1e7d98b1787a2c5befc4407fd407ccc
candinegits <candinegits@gmail.com>
2022-08-18 17:58:50 -0400

    steps to contribute

---
+++ andine.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+author:andine
+echo "steps to contribute to a project on github"
+echo " first i clone the remote repo to my local environment"
+echo "i create a sub branche for best practice"
+echo "then i make the updates on my codes"
+echo " i then do a git add,git commite -m and a git push"
+echo " then i assign it to a senior engineer for review"
+echo "once the code is reviewed i then sqash and merge to the main branch"
