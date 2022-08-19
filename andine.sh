bd4188a9cf9371ee1fe411c1cf4a4c27cf0c7c34
Lucky <ogegeson@gmail.com>
2022-08-18 19:38:10 -0500

    Update andine.sh

--- andine.sh
+++ andine.sh
@@ -2,8 +2,8 @@
 author:andine
 echo "steps to contribute to a project on github"
 echo " first i clone the remote repo to my local environment"
-echo "i create a sub branche for best practice"
-echo "then i make the updates on my codes"
-echo " i then do a git add,git commite -m and a git push"
-echo " then i assign it to a senior engineer for review"
-echo "once the code is reviewed i then sqash and merge to the main branch"
+echo "i create a sub branch for best practice"
+echo "then I make the updates on my codes"
+echo " I then do a git add,git commite -m and a git push"
+echo " then I assign it to a senior engineer for review"
+echo "once the code is reviewed, I then sqash and merge to the main branch"
